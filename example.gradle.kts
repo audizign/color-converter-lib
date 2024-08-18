40b22511c50ed2b36db91ac7e6a07d9d6ed109aa
Your Name <you@example.com>
2024-08-18 16:30:15 -0400

    cleanup

--- example.gradle.kts
+++ example.gradle.kts
@@ -3,40 +3,18 @@ plugins {
     id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
-val mcVersion = "1.16"
 group = "dev.idot"
 version = "0.1"
 
 repositories {
     mavenLocal()
-    mavenCentral()
-
-    maven("https://repo.codemc.io/repository/nms/")
-    maven("https://repo.codemc.io/repository/maven-public/")
-
-    maven("https://oss.sonatype.org/content/repositories/public/")
-    maven("https://hub.spigotmc.org/nexus/content/repositories/public/")
 }
 
 dependencies {
-    implementation("dev.idot:color-converter-lib:0.2") // this is a local dependency
-
-    compileOnly("org.bukkit:bukkit:$mcVersion+")
-    compileOnly("org.bukkit:craftbukkit:$mcVersion+")
-    compileOnly("org.spigotmc:spigot-api:$mcVersion+")
+    implementation("dev.idot:color-converter-lib:0.2")
 }
 
 tasks {
-    // if you're making a spigot plugin
-    processResources {
-        filesMatching("plugin.yml") {
-            expand(
-                "displayName" to project.name,  // name: ${displayName}
-                "version" to version            // version: ${version}
-            )
-        }
-    }
-    // this is for shading the dependencies
     shadowJar {
         archiveClassifier.set("")
         dependencies {
